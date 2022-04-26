a560a77812760842084ab17ae48739b9e00a83b6
dobi <dobi@kakao.com>
2022-04-26 15:50:15 +0900

    메인뷰컨 수정

---
+++ TwitterCloneApp/TwitterCloneApp/Controller/MainTabController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabController.swift
+//  TwitterCloneApp
+//
+//  Created by Ho bin Lee on 2022/04/26.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+
+}

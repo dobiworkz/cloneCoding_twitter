8bc2e5c7542b44cd4e5c1610394c3afed685c803
dobi <dobi@kakao.com>
2022-05-02 20:25:33 +0900

    프로파일이미지 파이어베이스에 등록하기

--- TwitterCloneApp/TwitterCloneApp/Controller/Authenitication/RegistrationController.swift
+++ TwitterCloneApp/TwitterCloneApp/Controller/Authenitication/RegistrationController.swift
@@ -11,6 +11,8 @@ import Firebase
 class RegistrationController: UIViewController{
     
     // MARK: - Properties
+    private let imagePicker = UIImagePickerController()
+    private var profileImage: UIImage?
     
     private let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
@@ -20,8 +22,6 @@ class RegistrationController: UIViewController{
         
         return button
     }()
-    
-    private let imagePicker = UIImagePickerController()
     
     private lazy var emailContainerView: UIView = {
         let image = #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
@@ -91,8 +91,8 @@ class RegistrationController: UIViewController{
         button.addTarget(self, action: #selector(handleShowLogin), for: .touchUpInside)
         return button
     }()
-
-
+    
+    
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
@@ -100,7 +100,7 @@ class RegistrationController: UIViewController{
         
         configureUI()
     }
-
+    
     // MARK: - Selectors
     
     @objc func handleAddProfilePhoto() {
@@ -112,22 +112,72 @@ class RegistrationController: UIViewController{
     }
     
     @objc func handleRegistration() {
-        guard let email = emailTextField.text else { return }
-        guard let password = passwordTextField.text else { return }
-        
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
-            if let error = error {
-                print("DEBUG: Error is \(error.localizedDescription)")
-                return
-            }
-            
-            print("DEBUG: Successfully registerd user")
-            
+        guard let profileImage = profileImage else {
+            print("Image error")
+            print(profileImage.debugDescription)
+            return
         }
         
+        print("====DEBUG====: set image ")
+        
+        // 이미지를 업로드하기위한 데이터로 변환
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
+        let fileName = NSUUID().uuidString
+        let storageRef = STORAGE_PROFILE_IMAGES.child(fileName)
+        
+        print("====DEBUG====: set filename ")
+        
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        guard let fullname = fullNameTextField.text else { return }
+        guard let username = userNameTextField.text else { return }
+        
+        // 이미지 업로드
+        storageRef.putData(imageData, metadata: nil) { meta, error in
+            
+            // 이미지 업로드 후 접근 url 가져옴
+            storageRef.downloadURL { url, error in
+                
+                if let error = error {
+                    print("DEBUG: Error is \(error.localizedDescription)")
+                    return
+                } else {
+                    print("DEBUG: Succesfully upload image")
+                }
+                
+                
+                guard let profileImageUrl = url?.absoluteString else { return }
+                
+                // 계정 추가
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    if let error = error {
+                        print("DEBUG: Error is \(error.localizedDescription)")
+                        return
+                    }
+                    
+                    guard let uid = result?.user.uid else { return }
+                    
+                    // 사용자의 정보를 딕셔너리로 전달하여 realtime database에 정보를 갱신한다.
+                    // 이유는 아직 정확하게 확인하지 못했으나 db의 위치를 us-central1으로 처리해야 정상동작한다. url구조가 조금 상이함.
+                    let values = ["email" : email,
+                                  "username" : username,
+                                  "fullname" : fullname,
+                                  "profileImageUrl": profileImageUrl]
+                    
+                    REF_USERS.child(uid).updateChildValues(values){ error, ref in
+                        if let error = error {
+                            print("DEBUG: Error is \(error.localizedDescription)")
+                            return
+                        } else {
+                            print("DEBUG: Succesfully registerd user")
+                        }
+                    }
+                }
+            }
+        }
     }
-
-
+    
+    
     // MARK: - Helpers
     func configureUI() {
         view.backgroundColor = UIColor.twitterBlue
@@ -153,7 +203,7 @@ class RegistrationController: UIViewController{
         view.addSubview(alreadyHaveAccountButton)
         alreadyHaveAccountButton.anchor(left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingLeft: 40, paddingRight: 40)
     }
-
+    
     
 }
 
@@ -165,6 +215,7 @@ extension RegistrationController: UIImagePickerControllerDelegate {
         // image에 렌더링을 해줘야 이미지 셋티잉 완료됨
         self.plusPhotoButton.setImage(profileImage.withRenderingMode(.alwaysOriginal), for: .normal)
         
+        self.profileImage = profileImage
         
         plusPhotoButton.layer.cornerRadius = 128/2
         plusPhotoButton.layer.masksToBounds = true
---
+++ TwitterCloneApp/TwitterCloneApp/Utils/Constance.swift
@@ -0,0 +1,15 @@
+//
+//  Constance.swift
+//  TwitterCloneApp
+//
+//  Created by Ho bin Lee on 2022/05/02.
+//
+
+import Firebase
+
+let DB_REF = Database.database().reference()
+let REF_USERS = DB_REF.child("users")
+
+let STORAGE_REF = Storage.storage().reference()
+let STORAGE_PROFILE_IMAGES = STORAGE_REF.child("profile_images")
+
